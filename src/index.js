66f8e88fd42ba273dd5bc8279461383670e500c1
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-28 10:45:56 -0600

    completed labwork

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 const init = () => {
+    const inputForm = document.querySelector("form")
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const info = document.querySelector("input#searchByID")
+
+        console.log(info.value)
+        fetch(`http://localhost:3000/movies/${info.value}`)
+            .then((resp) => resp.json())
+            .then((data) => {
+                const title = document.querySelector("section#movieDetails h4")
+                const summary = document.querySelector("section#movieDetails p")
+
+                title.innerText = data.title
+                summary.innerText = data.summary
+            })
+    })
   
 }
 
